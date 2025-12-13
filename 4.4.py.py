c81920dcde2aaee3e663d293a7ecba25a645df38
lamngo20062010-eng <lamngo20062010@gmail.com>
2025-12-13 08:13:34 +0700

    Delete 4.4.py.py

--- 4.4.py.py
+++
@@ -1,7 +0,0 @@
-print("Ngô Xuân Lâm")
-print("MSV: 245751030110040")
-print("5)")
-class py_solution:
-    def reverse_words(self, s):
-        return ' '.join(reversed(s.split()))
-print(py_solution().reverse_words("hello .py"))
